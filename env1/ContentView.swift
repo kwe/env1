5dc18e092ac4217bfc265e043b3fb6acf3b0eacf
Kevin Evans <kwevans@gmail.com>
2021-05-22 22:01:39 +0100

    Initial Commit

---
+++ env1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  env1
+//
+//  Created by Kevin Evans on 22/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ env1/env1App.swift
@@ -0,0 +1,17 @@
+//
+//  env1App.swift
+//  env1
+//
+//  Created by Kevin Evans on 22/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct env1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
